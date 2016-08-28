18ea51b72940aad512afaa8d098b10ca822863e3
Lucas Wiman <lucaswiman@counsyl.com>
2016-08-28 14:44:24 -0700

    Add django 1.10 support

--- django_hstore/descriptors.py
+++ django_hstore/descriptors.py
@@ -9,12 +9,17 @@ __all__ = [
 ]
 
 
-class HStoreDescriptor(models.fields.subclassing.Creator):
+class HStoreDescriptor(object):
     _DictClass = HStoreDict
 
     def __init__(self, *args, **kwargs):
         self.schema_mode = kwargs.pop('schema_mode', False)
         super(HStoreDescriptor, self).__init__(*args, **kwargs)
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        return obj.__dict__[self.field.name]
 
     def __set__(self, obj, value):
         value = self.field.to_python(value)
@@ -25,8 +30,13 @@ class HStoreDescriptor(models.fields.subclassing.Creator):
         obj.__dict__[self.field.name] = value
 
 
-class SerializedDictDescriptor(models.fields.subclassing.Creator):
+class SerializedDictDescriptor(object):
     _DictClass = dict
+
+    def __get__(self, obj, type=None):
+        if obj is None:
+            return self
+        return obj.__dict__[self.field.name]
 
     def __set__(self, obj, value):
         # Deserialization is only needed when retrieving data from DB
